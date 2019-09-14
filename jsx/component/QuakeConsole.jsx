6e22f603591f5f94c82af740c1c48df778f1a622
Nathan Grubb <me@nathangrubb.io>
2019-09-14 23:27:18 +0700

    Improve console

--- jsx/component/QuakeConsole.jsx
+++ jsx/component/QuakeConsole.jsx
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
-import KeyHandler, { KEYPRESS } from 'react-key-handler';
 import ReactTerminal, { ReactOutputRenderers, ReactThemes } from 'react-terminal-component';
 import {
   CommandMapping, OutputFactory, OutputType, EmulatorState,
   defaultCommandMapping
 } from 'javascript-terminal';
-
 import {Animated} from "react-animated-css";
-
 
 export default class QuakeConsole extends Component {
   constructor(props) {
     super(props);
-    this.terminalRef = React.createRef();
     this.state = { showConsole: false };
     this.toggleConsole = this.toggleConsole.bind(this);
     this.exitConsole = this.exitConsole.bind(this);
@@ -35,8 +31,7 @@ export default class QuakeConsole extends Component {
     };
 
     return (
-      <React.Fragment>
-        <div id="console" className="tilda" style={displayStyle}>
+      <div id="console" className="tilda" style={displayStyle}>
         <Animated animationIn="slideInDown" animationOut="slideOutUp" animationInDuration={500} animationOutDuration={500} isVisible={showConsole} animateOnMount={false}>
           <ReactTerminal
             theme={ReactThemes.hacker}
@@ -46,8 +41,7 @@ export default class QuakeConsole extends Component {
             emulatorState={this.customState}>
           </ReactTerminal>
         </Animated>
-        </div>
-      </React.Fragment>
+      </div>
     );
   }
 
