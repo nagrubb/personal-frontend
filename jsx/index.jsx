48a185f9a19c1f30fe3fee48b56466dd1e1d2cf4
Nathan Grubb <me@nathangrubb.io>
2020-01-23 03:59:07 +0700

    Fix some issues using material-ui

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -4,8 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
 import QuakeConsole from './component/QuakeConsole.jsx'
-import PictureWithName from './component/PictureWithName.jsx'
 import ContactDetails from './component/ContactDetails.jsx'
 import SkillDetails from './component/SkillDetails.jsx'
 import Languages from './component/Languages.jsx'
@@ -16,74 +16,60 @@ import Hobbies from './component/Hobbies.jsx'
 import CyclingGoals from './component/CyclingGoals.jsx'
 import VisitedPlaces from './component/VisitedPlaces.jsx'
 
-window.onkeydown = function(e) {
-  if (globalApp && globalApp.terminalRef) {
-    if (e.code == "Backquote") {
-      e.preventDefault();
-      globalApp.terminalRef.toggleConsole();
-    }
-  }
-}
+var terminalRef = null;
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   main: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
     background: '#f1f1f1',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+  }
 }));
 
-class ResumePage extends Component {
-  constructor(props) {
-    super(props);
-    this.terminalRef = null;
-  }
-
-  render() {
-    const classes = useStyles();
-    return (
-      <Container className={classes.main}>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={4}>
-            <Paper className={classes.paper}>
-              <div className="w3-white w3-text-grey w3-card-4" >
-                <PictureWithName />
-                <div className="w3-container">
-                  <ContactDetails />
-                  <hr />
-                  <SkillDetails />
-                  <hr />
-                  <Languages />
-                  <hr />
-                  <FinanceDetails />
-                  <CyclingGoals />
-                  <hr />
-                  <VisitedPlaces />
-                </div>
-              </div>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <Paper className={classes.paper}>
-              <WorkExperience />
-              <Education />
-              <Hobbies />
-            </Paper>
-          </Grid>
+function App() {
+  const classes = useStyles();
+  return (
+    <Container className={classes.main}>
+      <QuakeConsole ref={(input) => { terminalRef = input; }} />
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={4}>
+          <Paper>
+            <img src="images/profile.jpg" style={{width: '100%'}} alt="Nathan Grubb" />
+            <div className="w3-container">
+              <ContactDetails />
+              <Divider />
+              <SkillDetails />
+              <Divider />
+              <Languages />
+              <Divider />
+              <FinanceDetails />
+              <CyclingGoals />
+              <Divider />
+              <VisitedPlaces />
+            </div>
+          </Paper>
         </Grid>
-      </Container>
-    )
-  }
+        <Grid item xs={12} sm={8}>
+          <Paper>
+            <WorkExperience />
+            <Education />
+            <Hobbies />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
-var globalApp = ReactDOM.render(
-  <ResumePage />,
-  document.getElementById('root')
-);
+var globalApp = ReactDOM.render(<App />, document.getElementById('root'));
+
+window.onkeydown = function(e) {
+  if (terminalRef) {
+    if (e.code == "Backquote") {
+      e.preventDefault();
+      terminalRef.toggleConsole();
+    }
+  }
+}
