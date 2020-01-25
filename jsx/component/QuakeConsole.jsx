8fa6008b408132ad69ffbe70274aae3cd93f06bf
Nathan Grubb <ngrubb@axon.com>
2020-01-25 14:43:57 +0700

    More cleanup and fix hiding terminal

--- jsx/component/QuakeConsole.jsx
+++ jsx/component/QuakeConsole.jsx
@@ -1,12 +1,29 @@
 import React, {Component} from 'react'
 import ReactTerminal, { ReactOutputRenderers, ReactThemes } from 'react-terminal-component';
+import Box from '@material-ui/core/Box';
+import { withStyles } from "@material-ui/core/styles";
 import {
-  CommandMapping, OutputFactory, OutputType, EmulatorState,
+  CommandMapping,
+  OutputFactory,
+  OutputType,
+  EmulatorState,
   defaultCommandMapping
 } from 'javascript-terminal';
 import {Animated} from "react-animated-css";
 
-export default class QuakeConsole extends Component {
+const styles = theme => ({
+  tilda: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 1100,
+    display: 'block',
+    overflow: 'hidden',
+  },
+});
+
+class QuakeConsole extends Component {
   constructor(props) {
     super(props);
     this.state = { showConsole: false };
@@ -25,13 +42,8 @@ export default class QuakeConsole extends Component {
 
   render() {
     const { showConsole } = this.state;
-    var displayStyle = {
-      display: 'block',
-      overflow: 'hidden'
-    };
-
     return (
-      <div id="console" className="tilda" style={displayStyle}>
+      <Box id="console" className={this.props.classes.tilda}>
         <Animated animationIn="slideInDown" animationOut="slideOutUp" animationInDuration={500} animationOutDuration={500} isVisible={showConsole} animateOnMount={false}>
           <ReactTerminal
             theme={{
@@ -53,7 +65,7 @@ export default class QuakeConsole extends Component {
             emulatorState={this.customState}>
           </ReactTerminal>
         </Animated>
-      </div>
+      </Box>
     );
   }
 
@@ -66,3 +78,5 @@ export default class QuakeConsole extends Component {
     this.setState({ showConsole: !this.state.showConsole });
   }
 }
+
+export default withStyles(styles)(QuakeConsole);
